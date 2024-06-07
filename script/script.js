371c169020bc54d6f310d4aa81f4cbc922c85d92
SimanyeSTS <simanyesomdaka@gmail.com>
2024-06-07 12:37:05 +0200

    List

---
+++ script/script.js
@@ -0,0 +1,18 @@
+function addTask() {
+    var input = document.getElementById("taskInput");
+    var task = input.value;
+    if (task.trim() !== "") {
+        var ul = document.getElementById("taskList");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(task));
+        var deleteBtn = document.createElement("button");
+        deleteBtn.appendChild(document.createTextNode("X"));
+        deleteBtn.setAttribute("class", "delete-btn");
+        deleteBtn.onclick = function() {
+            ul.removeChild(li);
+        };
+        li.appendChild(deleteBtn);
+        ul.appendChild(li);
+        input.value = "";
+    }
+}
